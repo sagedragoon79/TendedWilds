28091107146abecc97641fb53cecac9577ed2c3b
SageDragoon <sagedragoon79@gmail.com>
2026-04-17 19:33:33 -0500

    Update AssemblyInfo file version to match mod version (1.0.5)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,12 +2,12 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TendedWilds")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tended Wilds")]
+[assembly: AssemblyDescription("Forager Shack overhaul mod for Farthest Frontier")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TendedWilds")]
-[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyCompany("SageDragoon")]
+[assembly: AssemblyProduct("Tended Wilds")]
+[assembly: AssemblyCopyright("Copyright © SageDragoon 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a7e3d1f0-5b2c-4a89-9d6e-1f4c8b3a2e07")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
